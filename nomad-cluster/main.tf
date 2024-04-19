3fc4824bdb221d43a9272577e1b567808d36adcf
Daniel Fedick <daniel.fedick@hashicorp.com>
2024-04-19 09:13:11 -0400

    Add mantic to the nodes

--- nomad-cluster/main.tf
+++ nomad-cluster/main.tf
@@ -14,6 +14,11 @@ terraform {
       source  = "hashicorp/vault"
       version = "~> 3.18.0"
     }
+
+    hcp = {
+      source  = "hashicorp/hcp"
+      version = "~> 0.66.0"
+    }
   }
 }
 
--- nomad-nodes/main.tf
+++ nomad-nodes/main.tf
@@ -88,16 +88,16 @@ data "terraform_remote_state" "nomad_cluster" {
   }
 }
 
-data "hcp_packer_image" "ubuntu_lunar_hashi_x86" {
-  bucket_name    = "ubuntu-lunar-hashi"
+data "hcp_packer_image" "ubuntu_mantic_hashi_x86" {
+  bucket_name    = "ubuntu-mantic-hashi"
   component_type = "amazon-ebs.amd"
   channel        = "latest"
   cloud_provider = "aws"
   region         = var.region
 }
 
-data "hcp_packer_image" "ubuntu_lunar_hashi_arm" {
-  bucket_name    = "ubuntu-lunar-hashi"
+data "hcp_packer_image" "ubuntu_mantic_hashi_arm" {
+  bucket_name    = "ubuntu-mantic-hashi"
   component_type = "amazon-ebs.arm"
   channel        = "latest"
   cloud_provider = "aws"
@@ -147,7 +147,7 @@ resource "aws_iam_instance_profile" "efs_instance_profile" {
 
 resource "aws_launch_template" "nomad_client_x86_launch_template" {
   name_prefix   = "lt-"
-  image_id      = data.hcp_packer_image.ubuntu_lunar_hashi_x86.cloud_image_id
+  image_id      = data.hcp_packer_image.ubuntu_mantic_hashi_x86.cloud_image_id
   instance_type = "t3a.medium"
 
   iam_instance_profile {
@@ -215,7 +215,7 @@ resource "aws_autoscaling_group" "nomad_client_x86_asg" {
 
 resource "aws_launch_template" "nomad_client_arm_launch_template" {
   name_prefix   = "lt-"
-  image_id      = data.hcp_packer_image.ubuntu_lunar_hashi_arm.cloud_image_id
+  image_id      = data.hcp_packer_image.ubuntu_mantic_hashi_arm.cloud_image_id
   instance_type = "t4g.medium"
 
   iam_instance_profile {
